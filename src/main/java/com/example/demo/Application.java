de70f34dc55ca920b1ddf326c8b99ffc5c712cd3
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-11 21:29:43 +0500

    Add pagination and sorting

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -17,41 +21,34 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student maria = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria.jones@amigoscode.edu",
-                    21
-            );
-            Student maria2 = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria2.jones@amigoscode.edu",
-                    25
-            );
-            Student ahmed = new Student(
-                    "Ahmed",
-                    "Ali",
-                    "ahmed.ali@amigoscode.edu",
-                    18
-            );
+            generateRandomStudents(studentRepository);
 
-            studentRepository.saveAll(List.of(maria, ahmed, maria2));
-
-            studentRepository
-                    .findStudentByEmail("ahmed.ali@amigoscode.edu")
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Student with email ahmed.ali@amigoscode.edu not found"));
-
-            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
-                    "Maria",
-                    21
-            ).forEach(System.out::println);
-
-            System.out.println("Deleting Maria 2");
-            System.out.println(studentRepository.deleteStudentById(3L));
+            PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
         };
+    }
+
+    private void sorting(StudentRepository studentRepository) {
+        Sort sort = Sort.by("firstName").ascending().and(Sort.by("age").descending());
+
+        studentRepository.findAll(sort)
+                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+            studentRepository.save(student);
+        }
     }
 
 }
