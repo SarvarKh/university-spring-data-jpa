7c3651f6d1e5cb38cab99906e02c5f0b007b3e9b
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-14 12:08:30 +0500

    Implement One To Many and vice versa relationship

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -34,17 +35,29 @@ public class Application {
                     email,
                     faker.number().numberBetween(17, 55));
 
+            student.addBook(new Book("Clean Code", LocalDateTime.now().minusDays(4)));
+            student.addBook(new Book("Think and Grow Rich", LocalDateTime.now()));
+            student.addBook(new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
             StudentIdCard studentIdCard = new StudentIdCard("123456789", student);
 
-            studentIdCardRepository.save(studentIdCard);
+            student.setStudentIdCard(studentIdCard);
+
+            studentRepository.save(student);
 
             studentRepository.findById(1L)
-                    .ifPresent(System.out::println);
-
-            studentIdCardRepository.findById(1L)
-                    .ifPresent(System.out::println);
-
-            studentRepository.deleteById(1L);
+                    .ifPresent(s -> {
+                        System.out.println("fetch book lazy...");
+                        List<Book> books = student.getBooks();
+                        books.forEach(book -> {
+                            System.out.println(s.getFirstName() + " borrowed " + book.getBookName());
+                        });
+                    });
+//
+//            studentIdCardRepository.findById(1L)
+//                    .ifPresent(System.out::println);
+//
+//            studentRepository.deleteById(1L);
         };
     }
 
---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,102 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Book")
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "book_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            name = "book_name",
+            nullable = false
+    )
+    private String bookName;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "BOOK_ID_STUDENT_ID_FK"
+            )
+    )
+    private Student student;
+
+    public Book() {
+    }
+
+    public Book(String bookName, LocalDateTime createdAt) {
+        this.bookName = bookName;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", bookName='" + bookName + '\'' +
+                ", student=" + student +
+                '}';
+    }
+}
